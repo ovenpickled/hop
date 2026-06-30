409483018e405c531f2cbca6a4ef88aabab2d39d
ovenpickled <aryanghate29@gmail.com>
2026-06-30 22:08:25 +0530

    add a routing file for API endpoints

--- main.go
+++ main.go
@@ -2,28 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/ovenpickled/hop/handler"
+	"github.com/ovenpickled/hop/router"
 	"github.com/ovenpickled/hop/store"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Welcome to the URL Shortener API",
-		})
-	})
-
-	r.POST("/create-short-url", func(c *gin.Context) {
-		handler.CreateShortUrl(c)
-	})
-
-	r.GET("/:shortUrl", func(c *gin.Context) {
-		handler.HandleShortUrlRedirect(c)
-	})
-
 	store.InitializeStore()
+
+	r := router.SetupRouter()
 
 	err := r.Run(":9808")
 	if err != nil {
